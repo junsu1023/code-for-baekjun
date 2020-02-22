2d271adb7a757072ff776e8a411d2088ce721f90
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-22 20:57:15 +0900

    백준 10950번

---
+++ Baekjun_10950.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjun_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test_case = sc.nextInt();
+        for(int tc=0; tc<test_case; tc++){
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(num1 + num2);
+        }
+    }
+}
