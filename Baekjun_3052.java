f6c44a7854d31e3b248d882275c3c36154307215
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-22 21:26:46 +0900

    백준 3052번

---
+++ Baekjun_3052.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Baekjun_3052 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int num = 0;
+        int[] save = new int [10];
+        int count = 1;
+        for(int i=0; i<10; i++){
+            num = input.nextInt();
+            save[i] = num % 42;
+        }
+        Arrays.sort(save);
+
+        for(int i=0; i<9; i++){
+            if(save[i]!=save[i+1])
+                count++;
+        }
+        System.out.println(count);
+    }
+}
