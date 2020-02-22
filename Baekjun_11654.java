23830a69c52412084bd0f84e4adfcb2369da6cd2
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-22 21:45:00 +0900

    백준 11654번

---
+++ Baekjun_11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baekjun_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int asc = input.charAt(0);
+        System.out.println(asc);
+    }
+}
