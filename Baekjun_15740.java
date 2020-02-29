7198c8f6fd574c0120470a464533bd366158dfc0
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 16:09:22 +0900

    백준 15740번

---
+++ Baekjun_15740.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Baekjun_15740 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] num = br.readLine().split(" ");
+        BigInteger num1 = new BigInteger(num[0]);
+        BigInteger num2 = new BigInteger(num[1]);
+        BigInteger addResult = num1.add(num2);
+        bw.write(addResult + "\n");
+        br.close();
+        bw.close();
+    }
+}
