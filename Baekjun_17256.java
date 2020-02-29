80cff31ea3bfc745e7376e139eae2c7f65b4e4b0
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 16:39:14 +0900

    백준 17256번

---
+++ Baekjun_17256.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjun_17256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[3];
+        int[] c = new int[3];
+        for(int i=0; i<3; i++) a[i] = sc.nextInt();
+        for(int i=0; i<3; i++) c[i] = sc.nextInt();
+        System.out.print(c[0]-a[2] + " " + c[1]/a[1] + " ");
+        System.out.print(c[2]-a[0]);
+    }
+}
