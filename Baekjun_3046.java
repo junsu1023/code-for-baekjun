e5fd26bd9750e2f7f398dd7d18f9912912a74df0
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 13:38:19 +0900

    백준 3046번

---
+++ Baekjun_3046.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baekjun_3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        System.out.println(2*S - R1);
+    }
+}
