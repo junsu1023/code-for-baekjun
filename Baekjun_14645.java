a1f31fed2544cad0090fbb2ec1d8e4b170389acf
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 15:33:14 +0900

    백준 14645번

---
+++ Baekjun_14645.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjun_14645 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int busstop = sc.nextInt();
+        int ride = sc.nextInt();
+        for(int i=0; i<busstop; i++){
+            int rides = sc.nextInt();
+            int take_off = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
