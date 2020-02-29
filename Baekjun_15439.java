39635b0ac611db04d71abc764362d18f99f64cd1
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 15:53:36 +0900

    백준 15439번

---
+++ Baekjun_15439.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Baekjun_15439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num*(num-1));
+    }
+}
