76e78dbe87571064068033b3fb63a02c1fa512da
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-29 13:40:38 +0900

    백준 5337번

---
+++ Baekjun_5337.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baekjun_5337 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
