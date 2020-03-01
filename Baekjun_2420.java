8d116e5ccc13fcac2c6b72b5be1ec4dd1f65c4ad
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-03-01 13:59:31 +0900

    백준 2420번

---
+++ Baekjun_2420.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Baekjun_2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num1 = sc.nextInt();
+        long num2 = sc.nextInt();
+        System.out.println(Math.abs(num2 - num1));
+    }
+}
