456a34c5f1bce4e98034571af4f8b7972d48313c
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-03-01 18:59:44 +0900

    백준 10768번

---
+++ Baekjun_10768.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baekjun_10768 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if(month == 2 && day == 18) System.out.println("Special");
+        else if(month<2 || (month==2 && day<18)) System.out.println("Before");
+        else if(month>2 || (month==2 && day>18)) System.out.println("After");
+    }
+}
