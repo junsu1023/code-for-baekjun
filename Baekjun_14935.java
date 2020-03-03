f68947fd480f29722f920b2af370b18251c6276b
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-03-03 21:26:59 +0900

    백준 14935번

---
+++ Baekjun_14935.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Baekjun_14935 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("FA");
+    }
+}
+// 모든 정수는 앞자리와 자리수를 곱하면 한자리가 된다.
