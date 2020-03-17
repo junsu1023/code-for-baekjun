1f78d2016001bd688012a008a9f541399db45c5d
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-03-17 22:24:06 +0900

    백준 6190번

---
+++ Baekjun_6190.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Baekjun_6190 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(n!=1){
+            if(n%2==0){
+                n/=2;
+                cnt++;
+            }
+            else{
+                n = 3*n+1;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
