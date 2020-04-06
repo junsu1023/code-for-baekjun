09eba137720e702df8041d3f643ae5334773066a
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-04-06 21:44:44 +0900

    백준 1568번

---
+++ Baekjun_1568.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Baekjun_1568 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int bird = 1;
+        int cnt = 0;
+        while(n >= 1){
+            if(n < bird) bird = 1;
+            n -= bird;
+            bird++;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
