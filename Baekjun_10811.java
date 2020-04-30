c75bf2117ed2e8185cffbcbee34377a868fa27bd
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-04-30 20:30:42 +0900

    백준 10811번

---
+++ Baekjun_10811.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Baekjun_10811 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = i+1;
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            a-=1;
+            b-=1;
+            while(a<=b){
+                int temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+                a++; b--;
+            }
+        }
+        for(int i=0; i<arr.length; i++) System.out.print(arr[i] + " ");
+    }
+}
