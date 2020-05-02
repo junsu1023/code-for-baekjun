56744169cf135c8200f2fe0487223b2191280eee
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-05-02 12:39:37 +0900

    백준 10821번

---
+++ Baekjun_10821.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Baekjun_10821 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(",");
+        System.out.println(arr.length);
+    }
+}
