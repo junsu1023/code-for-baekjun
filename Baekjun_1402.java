d20fc704c96009a5c178f6736c44f94073c73ff0
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-07-11 21:55:17 +0900

    백준 1402번

---
+++ Baekjun_1402.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjun_1402 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int test_case = sc.nextInt();
+        for(int tc=0; tc<test_case; tc++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("yes");
+        }
+    }
+}
