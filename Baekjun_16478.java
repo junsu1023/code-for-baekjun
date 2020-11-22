540e7942bd65202fa066c81ad283a1f9656ce125
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-11-22 14:13:21 +0900

    백준 16478번

---
+++ Baekjun_16478.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjun_16478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double r1 = Double.parseDouble(st.nextToken());
+        double r2 = Double.parseDouble(st.nextToken());
+        double r3 = Double.parseDouble(st.nextToken());
+        System.out.println((r1*r2*r3)/(r1*r2 + r2*r3 + r3*r1));
+    }
+}
